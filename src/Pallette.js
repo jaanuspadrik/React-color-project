47a0c66e12a215fa8e601e997c956975ea2ce7aa
Jaanus Padrik <jaanuspadrik@Jaanuss-MacBook-Pro.local>
2019-10-08 20:52:29 +0300

    color boxes

---
+++ src/Pallette.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import ColorBox from './ColorBox';
+import './Pallette.css';
+
+
+class Pallette extends Component {
+  render() {
+    const colorBoxes = this.props.colors.map(color => (
+      <ColorBox background={color.color} name={color.name}/>
+    ))
+    return (
+      <div className="Palette">
+        <div className="Palette-colors">
+          {colorBoxes}
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default Pallette;
