11b028c9c7ac95f31f778f31babd161dd6c66229
Jaanus Padrik <jaanus.padrik@hendrikson.ee>
2019-10-23 16:26:37 +0300

    Initial routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Route, Switch} from "react-router-dom";
 import Pallette from './Pallette';
 import seedColors from './seedColors';
 import {generatePalette} from "./colorHelpers";
 
 
-function App() {
-  return (
-    <div>
-      <Pallette palette={generatePalette(seedColors[4])}/>
-    </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function(palette){
+      return palette.id === id
+    })
+  };
+  render(){
+    return (
+      <Switch>
+        <Route exact path="/" render={() => <h1>Palette LIst</h1>}/>
+        <Route exact path="/palette/:id" render={routeProps => <Pallette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />}/>
+      </Switch>
+      //<div>
+        //<Pallette palette={generatePalette(seedColors[4])}/>
+      //</div>
+    );
+  }
 }
 
 export default App;
