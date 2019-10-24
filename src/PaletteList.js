594af4cf79a3497697ce290060add72346275974
Jaanus Padrik <jaanus.padrik@hendrikson.ee>
2019-10-24 16:42:56 +0300

    Styling minipalette list

---
+++ src/PaletteList.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import MiniPalette from "./MiniPalette";
+import {Link} from "react-router-dom";
+
+class PaletteList extends Component {
+  render() {
+    const {palettes} = this.props;
+    return (
+      <div>
+        <h1>React Colors</h1>
+        {palettes.map(palette => (
+          <MiniPalette {...palette} />
+        ))}
+      </div>
+    );
+  }
+
+}
+
+export default PaletteList;
