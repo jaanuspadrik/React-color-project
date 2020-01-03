04f6ee28b9824facba8c70513a659336106f5fb5
Jaanus Padrik <jaanus.padrik@hendrikson.ee>
2020-01-03 17:08:31 +0200

    randomColor and clearPalette

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -77,13 +77,16 @@ const styles = theme => ({
 });
 
 class NewPaletteForm extends Component {
+  static defaultProps = {
+    maxColors: 20
+  }
   constructor(props){
     super(props);
     this.state = {
       open: true,
       currentColor: "teal",
       newColorName: "",
-      colors: [],
+      colors: this.props.palettes[0].colors,
       newPaletteName: ""
     };
     this.updateCurrentColor = this.updateCurrentColor.bind(this);
@@ -91,6 +94,8 @@ class NewPaletteForm extends Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.removeColor = this.removeColor.bind(this);
+    this.clearColors = this.clearColors.bind(this);
+    this.addRandomColor = this.addRandomColor.bind(this);
   };
   componentDidMount(){
     ValidatorForm.addValidationRule("isColorNameUnique", value =>
@@ -132,6 +137,17 @@ class NewPaletteForm extends Component {
     this.setState({[evt.target.name]: evt.target.value})
   }
 
+  clearColors(){
+    this.setState({colors: []})
+  }
+
+  addRandomColor(){
+    const allColors = this.props.palettes.map(p => p.colors).flat();
+    var rand = Math.floor(Math.random() * allColors.length);
+    const randomColor = allColors[rand];
+    this.setState({colors: [...this.state.colors, randomColor]})
+  }
+
   handleSubmit(){
     let newName = this.state.newPaletteName;
     const newPalette = {
@@ -156,8 +172,9 @@ class NewPaletteForm extends Component {
   };
 
   render() {
-    const { classes } = this.props;
-    const { open } = this.state;
+    const { classes, maxColors } = this.props;
+    const { open, colors } = this.state;
+    const paletteIsFull = colors.length >= maxColors;
 
     return (
       <div className={classes.root}>
@@ -211,8 +228,8 @@ class NewPaletteForm extends Component {
           <Divider />
           <Typography variant="h4">Design Your Palette</Typography>
           <div>
-            <Button variant="contained" color="secondary">Clear Palette</Button>
-            <Button variant="contained" color="primary">Random Color</Button>
+            <Button variant="contained" color="secondary" onClick={this.clearColors}>Clear Palette</Button>
+            <Button variant="contained" color="primary" onClick={this.addRandomColor} disabled={paletteIsFull}>Random Color</Button>
           </div>
           <ChromePicker color={this.state.currentColor} onChangeComplete={this.updateCurrentColor}/>
           <ValidatorForm onSubmit={this.addNewColor}>
@@ -223,7 +240,7 @@ class NewPaletteForm extends Component {
               validators={["required", "isColorNameUnique", "isColorUnique"]}
               errorMessages={["Enter a color name", "Color name must be unique", "Color already used"]}
             />
-            <Button type="submit" variant="contained" color="primary" style={{backgroundColor: this.state.currentColor}}>Add Color</Button>
+            <Button type="submit" variant="contained" color="primary" disabled={paletteIsFull} style={{backgroundColor: paletteIsFull ? "grey" : this.state.currentColor}}>{paletteIsFull ? "Palette Full" : "Add Color"}</Button>
           </ValidatorForm>
         </Drawer>
         <main
